615dec6a8fdeab07438713790c648f7582938651
Jake <jakeroggenbuck2@gmail.com>
2024-09-12 00:11:43 -0700

    Add regression

---
+++ linear-regression/regression.h
@@ -0,0 +1,26 @@
+#ifndef REGRESSION_H
+#define REGRESSION_H
+
+#include <stdlib.h>
+
+struct DataFrame {
+    double *y;
+    double *x;
+    size_t size;
+};
+
+struct LinearComponents {
+    double slope;
+    double b;
+};
+
+double squared_error(struct DataFrame *dataf, double(f)(double));
+
+double mean_squared_error(struct DataFrame *dataf, double(f)(double));
+
+struct LinearComponents *gradient_descent(struct DataFrame *dataf, double slope,
+                                          double b, double learning_rate);
+
+struct LinearComponents *regression(int epochs, double learning_rate);
+
+#endif // REGRESSION_H

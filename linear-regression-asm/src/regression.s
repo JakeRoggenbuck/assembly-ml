3e6837430fdb8764f06418c011ddb0a0f5023f62
Jake <jakeroggenbuck2@gmail.com>
2024-09-16 19:57:04 -0700

    Add r_squared_error

--- linear-regression-asm/src/regression.s
+++ linear-regression-asm/src/regression.s
@@ -21,7 +21,7 @@
 #     double b;			8 bytes, 8
 # };
 
-r_squared_error:  													# double squared_error(struct DataFrame *dataf, double(f)(double)) {
+r_squared_error:  															# double squared_error(struct DataFrame *dataf, double(f)(double)) {
 	sub rsp, 32						# rsp -= 32
 
 	mov [rsp], r12					# save r12
@@ -31,24 +31,25 @@ r_squared_error:  													# double squared_error(struct DataFrame *dataf, d
 
 	mov r12, rdi					# save dataf to r12
 	mov r13, rsi					# save f to r13
-																	#     double error = 0.0;
+																			#     double error = 0.0;
 	mov r14, 0						# r14 = error, = 0
-																	#     for (int i = 0; i < dataf->size; i++) {
+																			#     for (int i = 0; i < dataf->size; i++) {
 	mov r15, 0						# r15 = i, = 0
-																	#         double delta = (dataf->y[i] - f(dataf->x[i]));
+																			#         double delta = (dataf->y[i] - f(dataf->x[i]));
 	mov [r12 + 8 + 8 * r10], rdi	# get dataf->x[i]
 	call r13						# call f on dataf->x[i]
 
-									# get dataf->y[i]
-									# subtract y value with f's output
-	
-																	#         error += delta * delta;
-									# multiply delta to delta
-									# add output to error (rax)
+	mov [r12 + 8 * r10], r11		# get dataf->y[i]
+	sub r11, rax					# subtract y value with f's output (in rax)
+	mov rax, r11					# move r11 over to rax
+																			#         error += delta * delta;
 
-																	#     }
-																	#     return error;
-																	# }
+	mul rax							# multipy delta (rax) by delta (rax)
+	add r14, rax					# add output to error
+
+																			#     }
+																			#     return error;
+																			# }
 	mov r12, [rsp]					# load r12
 	mov r13, [rsp+8] 				# load r13
 	mov r14, [rsp+16] 				# load r14

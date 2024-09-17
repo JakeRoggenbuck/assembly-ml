53195c7cad4cd81d53e19276fa54be672630ed37
Jake <jakeroggenbuck2@gmail.com>
2024-09-16 19:39:08 -0700

    Start to add assembly for first function

--- linear-regression-asm/src/regression.s
+++ linear-regression-asm/src/regression.s
@@ -11,29 +11,51 @@
 	.globl r_regression
 
 # struct DataFrame {
-#     double *y;
-#     double *x;
-#     size_t size;
+#     double *y;		8 bytes, 0
+#     double *x;		8 bytes, 8
+#     size_t size;		8 bytes, 16
 # };
 
 # struct LinearComponents {
-#     double slope;
-#     double b;
+#     double slope;		8 bytes, 0
+#     double b;			8 bytes, 8
 # };
 
-r_squared_error:  											# double squared_error(struct DataFrame *dataf, double(f)(double)) {
-															#     double error = 0.0;
-	mov rax, 0			# rax = error, = 0
-															#     for (int i = 0; i < dataf->size; i++) {
-	mov r10, 0			# r10 = i, = 0
-															#         double delta = (dataf->y[i] - f(dataf->x[i]));
+r_squared_error:  													# double squared_error(struct DataFrame *dataf, double(f)(double)) {
+	sub rsp, 32						# rsp -= 32
+
+	mov [rsp], r12					# save r12
+	mov [rsp+8], r13				# save r13
+	mov [rsp+16], r14				# save r14
+	mov [rsp+24], r15				# save r15
+
+	mov r12, rdi					# save dataf to r12
+	mov r13, rsi					# save f to r13
+																	#     double error = 0.0;
+	mov r14, 0						# r14 = error, = 0
+																	#     for (int i = 0; i < dataf->size; i++) {
+	mov r15, 0						# r15 = i, = 0
+																	#         double delta = (dataf->y[i] - f(dataf->x[i]));
+	mov [r12 + 8 + 8 * r10], rdi	# get dataf->x[i]
+	call r13						# call f on dataf->x[i]
+
+									# get dataf->y[i]
+									# subtract y value with f's output
 	
-															#         error += delta * delta;
-															#     }
-															# 
-															#     return error;
-															# }
-ret
+																	#         error += delta * delta;
+									# multiply delta to delta
+									# add output to error (rax)
+
+																	#     }
+																	#     return error;
+																	# }
+	mov r12, [rsp]					# load r12
+	mov r13, [rsp+8] 				# load r13
+	mov r14, [rsp+16] 				# load r14
+	mov r15, [rsp+24] 				# load r15
+
+	add rsp, 32						# rsp += 32
+ret									# return rax with error value
 
 r_mean_squared_error:
 															# double mean_squared_error(struct DataFrame *dataf, double(f)(double)) {
